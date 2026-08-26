c208415424929f9589b7c2b7b75daacf165e3710
Ken Tucker <vb2ae@live.com>
2026-08-26 07:18:37 -0400

    Delete vb2ae.ServiceLocator.MSDependencyInjection.xunit directory

--- vb2ae.ServiceLocator.MSDependencyInjection.xunit/Models/IService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace vb2ae.ServiceLocator.MSDependencyInjection.xunit.Models
-{
-    public interface IService
-    {
-        void DoSomething();
-    }
-}
--- vb2ae.ServiceLocator.MSDependencyInjection.xunit/Models/Toyota.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace vb2ae.ServiceLocator.MSDependencyInjection.xunit.Models
-{
-    public class Toyota : ICar
-    {
-        public string Name { get; set; } = "Toyota";
-    }
-}
